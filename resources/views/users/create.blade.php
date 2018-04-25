80fb3a9bdb12f18ee2088996f6e6a1ecfbacd4e3
joel <joelcluna@gmail.com>
2018-04-25 10:08:19 -0500

    agregue otros roles de validacion

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -5,17 +5,32 @@
 @section('content')
     <h1 class="header" >Crear Usuario</h1>
 
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <h6>Por favor corrige los siguientes errores:</h6>
+           {{-- <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>--}}
+        </div>
+    @endif
+
     <form method="POST" action="{{ url('usuarios') }}">
         {{csrf_field() }}
 
 
         <div class="form-group">
             <label for="name">Nombre:</label>
-            <input type="text" class="form-control" name="name" id="name">
+            <input type="text" class="form-control" name="name" id="name" value="{{old('name')}}">
+            @if ($errors->has('name'))
+                <p>{{$errors->first('name')}}</p>
+            @endif
+
         </div>
         <div class="form-group">
             <label for="email">Correo Electronico:</label>
-            <input type="email" class="form-control" name="email" id="email">
+            <input type="email" class="form-control" name="email" id="email" value="{{old('name')}}">
         </div>
         <div class="form-group">
             <label for="pwd">Contraseña:</label>
@@ -26,6 +41,7 @@
                 <input class="form-check-input" type="checkbox">Recordar Datos
             </label>
         </div>
+
 
         <button type="submit" class="btn btn-primary">Crear Usuario</button>
     </form>
