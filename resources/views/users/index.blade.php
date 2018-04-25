3a1b3729f0b5df91467ba67d488df8b3458c59cf
joel <joelcluna@gmail.com>
2018-04-25 16:39:48 -0500

    estilos a la tabla de usuarios

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,41 +3,48 @@
 @section('title', 'Usuarios')
 
 @section('content')
-    <h1>{{ $title }}</h1>
-    <p><a  class="btn btn-primary" href="{{ route('users.create') }}"> Nuevo Usuario</a> </p>
+    <div class="d-flex justify-content-between align-items-end mb-3">
 
-<table class="table table-striped">
-    <thead class="thead-dark">
-    <tr>
-        <th scope="col">#</th>
-        <th scope="col">Nombre</th>
-        <th scope="col">Correo Electronico</th>
-        <th scope="col">Ver</th>
-        <th scope="col">Editar Usuario</th>
-        <th scope="col">Eliminar</th>
-    </tr>
-    </thead>
-    <tbody>
-    @forelse ($users as $user)
+        <h1 class="pb-1">{{ $title }}</h1>
+        <p><a  class="btn btn-primary" href="{{ route('users.create') }}"> Nuevo Usuario</a> </p>
+
+    </div>
+
+    @if ($users->isNotEmpty())
+    <table class="table table-striped">
+        <thead class="thead-dark">
         <tr>
-            <th scope="row">{{ $user->id }}</th>
-            <th>{{ $user->name }}</th>
-            <th>{{ $user->email }} </th>
-            <th><a class="btn btn-success" href="{{ route('users.show', $user) }}">Detalle</a></th>
-            <th><a class="btn btn-warning" href="{{ route('users.edit', $user) }}">Editar</a> </th>
-            <form action="{{ route('users.destroy', $user) }}" method="POST">
-                {{csrf_field()}}
-                {{method_field('DELETE')}}
-                <th><button type="submit" class="btn btn-danger">Eliminar</button> </th>
-            </form>
-
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Correo Electronico</th>
+            <th scope="col">Ver</th>
+            <th scope="col">Editar Usuario</th>
+            <th scope="col">Eliminar</th>
         </tr>
+        </thead>
+        <tbody>
+        @forelse ($users as $user)
+            <tr>
+                <th scope="row">{{ $user->id }}</th>
+                <th>{{ $user->name }}</th>
+                <th>{{ $user->email }} </th>
+                <th><a class="btn btn-link" href="{{ route('users.show', $user) }}"><span class="oi oi-eye"></span></a></th>
+                <th><a class="btn btn-link" href="{{ route('users.edit', $user) }}"><span class="oi oi-pencil"></span></a> </th>
+                <form action="{{ route('users.destroy', $user) }}" method="POST">
+                    {{csrf_field()}}
+                    {{method_field('DELETE')}}
+                    <th><button class="btn btn-link" type="submit"><span class="oi oi-trash"></span></button> </th>
+                </form>
 
+            </tr>
         @empty
             <li>No hay usuarios registrados.</li>
-        @endforelse
-    </tbody>
-</table>
+            @endforelse
+        </tbody>
+    </table>
+@else
+    <li>No hay usuarios registrados.</li>
+@endif
     @endsection
 
 @section('sidebar')
